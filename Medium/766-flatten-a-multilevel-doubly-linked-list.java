8a16989d7e37efd4522853c872a0d6426e2458a6
Dadasaheb Adsul <167906335+dadasahebadsul@users.noreply.github.com>
2026-07-20 10:36:38 +0530

    [LeetCode] 766. Flatten a Multilevel Doubly Linked List (Medium)

--- Medium/766-flatten-a-multilevel-doubly-linked-list.java
+++ Medium/766-flatten-a-multilevel-doubly-linked-list.java
@@ -1,6 +1,6 @@
 // 766. Flatten a Multilevel Doubly Linked List (Medium)
 // https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
-// Runtime: 0 ms  Memory: 43.4 MB
+// Runtime: 0 ms  Memory: 43.8 MB
 /*
 // Definition for a Node.
 class Node {
@@ -22,27 +22,30 @@ class Solution {
     public Node dfs(Node head){
         Node curr=head;
         Node last=null;
-        while(curr!=null){
-            Node next=curr.next;
-            if(curr.child!=null){
-                Node childHead=curr.child;
-                Node childTail=dfs(childHead);
+       while(curr!=null){
+        Node next=curr.next;
+        if(curr.child!=null){
+            Node childHead=curr.child;
+            Node childTail=dfs(childHead);
 
-                curr.next=childHead;
-                childHead.prev=curr;
+            curr.next=childHead;
+            childHead.prev=curr;
 
-                curr.child=null;
-                if(next!=null){
-                    childTail.next=next;
-                    next.prev=childTail;
-                }
-                last=childTail;
-                curr=childTail;
-            }else{
-                last=curr;
+            curr.child=null;
+
+            if(next!=null){
+                childTail.next=next;
+                next.prev=childTail;
             }
+            last=childTail;
+            curr=childTail;
+
+
+        }else{
+            last=curr;
+           }
             curr=curr.next;
-        }
-        return last;
+       }
+       return last;
     }
 }
