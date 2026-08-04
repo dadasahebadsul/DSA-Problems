a72a69179e4fda6213a106430e821547e9fe7416
Dadasaheb Adsul <167906335+dadasahebadsul@users.noreply.github.com>
2026-08-04 13:14:52 +0530

    [LeetCode] 496. Next Greater Element I (Easy)

---
+++ Easy/496-next-greater-element-i.java
@@ -0,0 +1,26 @@
+// 496. Next Greater Element I (Easy)
+// https://leetcode.com/problems/next-greater-element-i/
+// Runtime: 4 ms  Memory: 45.3 MB
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int []ans=new int[nums1.length];
+        Stack<Integer>s=new Stack<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek()<=nums2[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                map.put(nums2[i],-1);
+            }else{
+                map.put(nums2[i],s.peek());
+            }
+            s.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.length;i++){
+            ans[i]=map.get(nums1[i]);
+        }
+        return ans;
+
+    }
+}
